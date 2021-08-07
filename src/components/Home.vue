598f6aeb439ef1628d81f3c992c4f284ba044ffc
Qindsx <1714797713@qq.com>
2021-08-07 11:08:15 +0800

    git

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,48 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/ass.jpg" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <!-- 侧边栏菜单区 -->
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu index="1">
+            <!-- 一级菜单模板 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-location"></i>
+              <!-- 文本 -->
+              <span>导航一</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="1-4-1">
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-location"></i>
+                <!-- 文本 -->
+                <span>导航一</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -15,4 +56,34 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 3px;
+  align-items: center;
+}
+.el-header div {
+  display: flex;
+  align-items: center;
+}
+.el-header img {
+  width: 50px;
+  height: 50px;
+}
+.el-header span {
+  font-size: 20px;
+  margin-left: 15px;
+  color: #fff;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
+</style>
