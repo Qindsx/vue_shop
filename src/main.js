01775811d921ee227f41dd093681c7e97c4b3591
Qindsx <1714797713@qq.com>
2021-08-08 21:54:02 +0800

    done userlist

--- src/main.js
+++ src/main.js
@@ -10,8 +10,12 @@ import './assets/css/global.css'
 
 // 导入axios
 import axios from 'axios'
-Vue.prototype.$http = axios;
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/';
+axios.interceptors.request.use(config =>{
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,40 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Login from '../components/Login.vue'
-import Home from '../components/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Login from "../components/Login.vue";
+import Home from "../components/Home.vue";
+import Welcome from "../components/Welcome.vue";
+import Users from "../components/user/Users.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
- const router =  new Router({
+const router = new Router({
   routes: [
+    { path: "/", redirect: "/login" },
+    { path: "/login", component: Login },
     {
-      path: '/',
-      redirect: '/login'
-    },
-    {
-      path: '/login',
-      component: Login
-    },
-    {
-      path: '/home',
-      component: Home
+      path: "/home",
+      component: Home,
+      redirect: "/welcome",
+      children: [
+        { path: "/home", component: Welcome },
+        { path: "/users", component: Users }
+      ]
     }
   ]
-})
+});
 
 // 挂载路由导航守卫
-router.beforeEach((to, from, next) =>{
+router.beforeEach((to, from, next) => {
   // to 将要访问的路径
   // from 从哪个路径跳转
   // next 是一个函数，表示放行
   //    next() next('/login')  强制跳转
 
-  if(to.path === '/login') return next()
+  if (to.path === "/login") return next();
   // 获取token
-  const tokenStr = window.sessionStorage.getItem('token');
-  if(!tokenStr) return next('/login');
+  const tokenStr = window.sessionStorage.getItem("token");
+  if (!tokenStr) return next("/login");
   next();
-})
+});
 
-export default router
+export default router;
